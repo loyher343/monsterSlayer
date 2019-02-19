639029b027d6a285d2a0b02892e580335ca2c378
unknown <loyhernandez59@gmail.com>
2019-02-19 01:04:55 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function() {
+        
+           
+            this.monsterHealth -= this.calculateDamage(3,10);
+
+            if (this.monsterHealth <= 0) {
+                alert('You Won!');
+                this.gameIsRunning = false;
+                return
+            }
+            
+            if (this.checkWin()) {
+                return;
+            }
+            this.playerHealth -= this.calculateDamage(5,12);
+
+            if (this.playerHealth <= 0) {
+                alert('You lost! :( ');
+                this.gameIsRunning = false;
+            
+            }
+            this.checkWin();
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
+        },
+        calculateDamage: function(min, max) {
+            return  Math.max(Math.floor(Math.random() * max) + 1, min)
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You Won! New Game?>')) {
+                    this.startGame();
+                } else {
+                    his.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <=0 ){
+                if (confirm('You Lost! New Game?>')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+
+            }
+            return false;
+        }
+    }
+
+})
