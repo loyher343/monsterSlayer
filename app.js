502cd7a1dce11c1daeb4d4b676606e8519b2e7ea
unknown <loyhernandez59@gmail.com>
2019-02-19 01:12:28 -0600

    fixed end game bug and added special attack

--- app.js
+++ app.js
@@ -12,29 +12,18 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function() {
-        
-           
             this.monsterHealth -= this.calculateDamage(3,10);
-
-            if (this.monsterHealth <= 0) {
-                alert('You Won!');
-                this.gameIsRunning = false;
-                return
-            }
-            
             if (this.checkWin()) {
                 return;
             }
-            this.playerHealth -= this.calculateDamage(5,12);
-
-            if (this.playerHealth <= 0) {
-                alert('You lost! :( ');
-                this.gameIsRunning = false;
-            
-            }
-            this.checkWin();
+            this.monsterAttacks();
         },
         specialAttack: function() {
+            this.monsterHealth -= this.calculateDamage(10,20);
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
 
         },
         heal: function() {
@@ -42,6 +31,10 @@ new Vue({
         },
         giveUp: function() {
 
+        },
+        monsterAttacks: function() {
+            this.playerHealth -= this.calculateDamage(5,12);
+            this.checkWin();
         },
         calculateDamage: function(min, max) {
             return  Math.max(Math.floor(Math.random() * max) + 1, min)
@@ -51,7 +44,7 @@ new Vue({
                 if (confirm('You Won! New Game?>')) {
                     this.startGame();
                 } else {
-                    his.gameIsRunning = false;
+                    this.gameIsRunning = false;
                 }
                 return true;
             } else if (this.playerHealth <=0 ){
